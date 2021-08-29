8d168a0b59a0d59ebac02eb035fa5496ac1ca094
SamiPerttu <Sami.Perttu@gmail.com>
2021-08-29 07:18:42 +0300

    Add inlining.

--- src/vec.rs
+++ src/vec.rs
@@ -57,35 +57,45 @@ pub type Int2 = glam::IVec2;
 pub type Int3 = glam::IVec3;
 pub type Int4 = glam::IVec4;
 
+#[inline]
 pub fn vec2(x: f32, y: f32) -> Vec2 {
     Vec2::new(x, y)
 }
+#[inline]
 pub fn vec3(x: f32, y: f32, z: f32) -> Vec3 {
     Vec3::new(x, y, z)
 }
+#[inline]
 pub fn vec3a(x: f32, y: f32, z: f32) -> Vec3a {
     Vec3a::new(x, y, z)
 }
+#[inline]
 pub fn vec4(x: f32, y: f32, z: f32, w: f32) -> Vec4 {
     Vec4::new(x, y, z, w)
 }
 
+#[inline]
 pub fn vec2d(x: f64, y: f64) -> Vec2d {
     Vec2d::new(x, y)
 }
+#[inline]
 pub fn vec3d(x: f64, y: f64, z: f64) -> Vec3d {
     Vec3d::new(x, y, z)
 }
+#[inline]
 pub fn vec4d(x: f64, y: f64, z: f64, w: f64) -> Vec4d {
     Vec4d::new(x, y, z, w)
 }
 
+#[inline]
 pub fn int2(x: i32, y: i32) -> Int2 {
     Int2::new(x, y)
 }
+#[inline]
 pub fn int3(x: i32, y: i32, z: i32) -> Int3 {
     Int3::new(x, y, z)
 }
+#[inline]
 pub fn int4(x: i32, y: i32, z: i32, w: i32) -> Int4 {
     Int4::new(x, y, z, w)
 }
@@ -99,12 +109,15 @@ pub trait Vec2Ext {
 
 impl Vec2Ext for Vec2 {
     type Scalar = f32;
+    #[inline]
     fn from_angle(radians: f32) -> Vec2 {
         vec2(cos(radians), sin(radians))
     }
+    #[inline]
     fn rotate_90(self) -> Self {
         vec2(-self.y, self.x)
     }
+    #[inline]
     fn rotate_270(self) -> Self {
         vec2(self.y, -self.x)
     }
@@ -112,12 +125,15 @@ impl Vec2Ext for Vec2 {
 
 impl Vec2Ext for Vec2d {
     type Scalar = f64;
+    #[inline]
     fn from_angle(radians: f64) -> Vec2d {
         vec2d(cos(radians), sin(radians))
     }
+    #[inline]
     fn rotate_90(self) -> Self {
         vec2d(-self.y, self.x)
     }
+    #[inline]
     fn rotate_270(self) -> Self {
         vec2d(self.y, -self.x)
     }
@@ -125,12 +141,15 @@ impl Vec2Ext for Vec2d {
 
 impl Vec2Ext for Int2 {
     type Scalar = i32;
+    #[inline]
     fn from_angle(_radians: i32) -> Int2 {
         panic!()
     }
+    #[inline]
     fn rotate_90(self) -> Self {
         int2(-self.y, self.x)
     }
+    #[inline]
     fn rotate_270(self) -> Self {
         int2(self.y, -self.x)
     }
